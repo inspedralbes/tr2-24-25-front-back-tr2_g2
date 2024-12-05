a2f773b523257fe102a34bf48ed0abbdaf5aaf57
julsluks <a22betvilver@inspedralbes.cat>
2024-12-05 20:01:39 +0100

    Add new view components for main, profile, report, chat, validation, and chat list pages; remove old page files

--- front/appMentoria/src/router/index.js
+++ front/appMentoria/src/router/index.js
@@ -1,22 +1,45 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import mainPage from '../views/mainPage.vue'
+// import profile from '../views/profile.vue'
+// import reportPage from '../views/reportPage.vue'
+// import validationPage from '../views/validationPage.vue'
+// import chatListPage from '../views/chatListPage.vue'
+// import chatPage from '../views/chatPage.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: HomeView,
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue'),
-    },
+      name: 'mainPage',
+      component: mainPage,
+    }
+    // {
+    //   path: '/profile',
+    //   name: 'profile',
+    //   component: profile,
+    // },
+    // {
+    //   path: '/report',
+    //   name: 'reportPage',
+    //   component: reportPage,
+    // },
+    // {
+    //   path: '/validation',
+    //   name: 'validationPage',
+    //   component: validationPage,
+    // },
+
+    // {
+    //   path: '/chatList',
+    //   name: 'chatListPage',
+    //   component: chatListPage,
+    // },
+    // {
+    //   path: '/chat',
+    //   name: 'chatPage',
+    //   component: chatPage, 
+    // },
   ],
 })
 
---
+++ front/appMentoria/src/views/mainPage.vue
@@ -0,0 +1,18 @@
+<template>
+
+    <Header></Header>
+    <h1>Hola soy Main</h1>
+
+    <NavBar></NavBar>
+</template>
+
+
+
+<script setup>
+
+import { ref } from 'vue'
+
+import NavBar from '@/components/NavBar.vue';
+import Header from '@/components/header.vue';
+
+</script>
