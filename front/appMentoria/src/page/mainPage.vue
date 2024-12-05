ffc5d33cc72fa33c2ab8f75ba4c64f989903fba5
SaraMartinez21 <a21sarmarbau@inspedralbes.cat>
2024-12-05 11:28:03 +0100

    Refactor App.vue and add NavBar component; remove unused pages and update router configuration

--- front/appMentoria/src/page/mainPage.vue
+++
@@ -1,4 +0,0 @@
-<template>
-
-    <h1>Hola soy Main</h1>
-</template>
---
+++ front/appMentoria/src/views/mainPage.vue
@@ -0,0 +1,15 @@
+<template>
+
+    <h1>Hola soy Main</h1>
+
+    <NavBar></NavBar>
+</template>
+
+
+
+<script setup>
+
+import { ref } from 'vue'
+import NavBar from '@/components/NavBar.vue';
+
+</script>
