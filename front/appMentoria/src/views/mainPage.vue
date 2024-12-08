3b112bf87f11c113c3ba65d8e0f5fe39ea06ac35
SaraMartinez21 <123445716+SaraMartinez21@users.noreply.github.com>
2024-12-08 13:50:31 +0100

    Add ViewPostList component and integrate it into mainPage; fetch posts from JSON file and display them

--- front/appMentoria/src/views/mainPage.vue
+++ front/appMentoria/src/views/mainPage.vue
@@ -1,18 +1,19 @@
 <template>
-
-    <Header></Header>
-    <h1>Hola soy Main</h1>
-
-    <NavBar></NavBar>
-</template>
+    <div class="flex flex-col min-h-screen">
+      <Header class="fixed top-0 left-0 right-0 z-10"></Header>
+      <main class="flex-grow mt-16 mb-60">
+        <viewPostList></viewPostList>
+      </main>
+      <NavBar></NavBar>
+    </div>
+  </template>
 
 
 
 <script setup>
 
-import { ref } from 'vue'
-
 import NavBar from '@/components/NavBar.vue';
-import Header from '@/components/header.vue';
+import Header from '@/components/Header.vue';
+import viewPostList from '@/components/ViewPostList.vue';
 
 </script>
