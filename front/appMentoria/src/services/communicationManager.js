610f73640f5a36ffaafe05728a65de9b47831607
julsluks <a22betvilver@inspedralbes.cat>
2024-12-19 22:56:25 +0100

    aaa

--- front/appMentoria/src/services/communicationManager.js
+++ front/appMentoria/src/services/communicationManager.js
@@ -4,6 +4,23 @@ const COMMUNITY_URL = import.meta.env.VITE_URL_BACK_COMMUNITY;
 const EMPLOYMENTEXCHANGE_URL = import.meta.env.VITE_URL_BACK_EMPLOYMENTEXCHANGE;
 const STADISTICS_URL = import.meta.env.VITE_URL_BACK_STADISTICS;
 
+// Login API firebase
+export const loginAPI = async (user) => {
+    try {
+        const response = await fetch(`${BACK_URL}/login`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(user),
+        });
+
+        return response.json();
+    } catch (error) {
+        console.error(error);
+    }
+};
+
 // Create publications
 export const postCommunityPublication = async (publication) => {
     try {
