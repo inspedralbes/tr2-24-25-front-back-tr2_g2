4ac3c40bcab029a298a97c894b3d08b09f466f22
a23cliferand <a23cliferand@inspedralbes.cat>
2024-12-21 14:35:25 +0100

    Add MongoDB integration and update chat service to use Mongoose

--- back/node/services/chat/index.js
+++ back/node/services/chat/index.js
@@ -5,21 +5,58 @@ const bcrypt = require('bcryptjs');
 const mysql = require('mysql2');
 const path = require('path');
 const cors = require('cors');
+const mongoose = require('mongoose');
 require('dotenv').config();
 
 const app = express();
-const port = process.env.PORT;
 
 app.use(express());
 app.use(cors());
 
+console.log(process.env.MONGO_URI);
 
-// Define a simple route
-app.get('/', (req, res) => {
-    res.send('Hello World!');
+mongoose.connect(process.env.MONGO_URI, { dbName: 'Chat' })
+    .then(() => console.log('Conexión exitosa a MongoDB'))
+    .catch(err => console.error('Error de conexión a MongoDB:', err));
+
+const messageSchema = new mongoose.Schema({
+        user_one_id: Number,
+        user_two_id: Number,
+        interactions: [
+                {
+                        userId: String,
+                        message: String,
+                        timestamp: Date
+                }
+        ]
 });
 
-// Start the server
-app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
+const Message = mongoose.model('Data', messageSchema);
+
+app.get('/getChats', async (req, res) => {
+    try {
+        console.log(await Message.find())
+        const messages = await Message.find();
+        console.log(messages);
+        res.json(messages);
+    } catch (err) {
+        res.status(500).send(err);
+    }
+});
+
+
+app.get('/getChats/:id', async (req, res) => {
+    const id = Number(req.params.id);
+    try {
+        const messages = await Message.find({ $or: [{ user_one_id: id }, { user_two_id: id }] });
+        console.log(messages);
+        res.json(messages);
+    } catch (err) {
+        console.error(err);
+        res.status(500).send(err);
+    }
+});
+
+app.listen(3004, () => {
+    console.log('listening on *: ' + 3004);
 });
