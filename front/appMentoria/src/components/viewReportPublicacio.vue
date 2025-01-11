c61093a1fde1e03ed05425c881c017e9274726c3
SaraMartinez21 <123445716+SaraMartinez21@users.noreply.github.com>
2025-01-11 17:37:09 +0100

    Enhance report user view: update styling, improve layout, and refine report status handling

--- front/appMentoria/src/components/viewReportPublicacio.vue
+++ front/appMentoria/src/components/viewReportPublicacio.vue
@@ -1,100 +1,108 @@
 <template>
-    <div class="container mx-auto p-3 sm:p-4 bg-white shadow rounded-lg">
-      <h1 class="text-lg sm:text-xl font-bold mb-3">Publicacions Reportades</h1>
-      <div>
-        <div v-for="report in reports" :key="report.id" class="border border-gray-300 rounded-lg p-3 mb-3 shadow-sm">
-          <div class="flex flex-wrap justify-between">
-            <div class="w-1/2 sm:w-1/3">
-              <p class="text-xs font-semibold">Informe Nº:</p>
-              <p class="text-gray-700 text-sm">{{ report.id }}</p>
-            </div>
-            <div class="w-1/2 sm:w-1/3">
-              <p class="text-xs font-semibold">Estat:</p>
-              <select v-model="report.status" @change="updateReportStatus(report.id, report.status)"
-              class="appearance-none bg-gray-100 border border-gray-300 text-sm rounded-md focus:outline-none focus:ring-2 focus:ring-blue-400 focus:border-blue-400 p-2 w-32 shadow-sm">
-                <option value="pending">Pending</option>
-                <option value="revising">Revising</option>
-                <option value="revised">Reviset</option>
-              </select>
-            </div>
-            <div class="w-full sm:w-1/3 mt-4">
-              <p class="text-xs font-semibold">Data:</p>
-              <p class="text-gray-700 text-sm">{{ report.created_at }}</p>
-            </div>
+  <div class="container mx-auto p-3 sm:p-4 bg-white dark:bg-gray-700 shadow rounded-lg">
+    <h1 class="text-lg sm:text-xl font-bold mb-3 text-gray-900 dark:text-gray-100">Publicacions Reportades</h1>
+    <div>
+      <div v-for="report in reports" :key="report.id"
+        class=" border border-gray-300 dark:border-gray-300 rounded-lg p-3 mb-3 shadow-sm dark:bg-gray-600">
+        <div class="flex flex-wrap justify-between mb-4">
+          <div class="w-1/2 sm:w-1/3 mb-4">
+            <p class="text-lg font-semibold text-gray-900 dark:text-gray-300">Informe Nº:</p>
+            <p class="text-gray-700 dark:text-gray-400 text-base">{{ report.id }}</p>
           </div>
-          <div class="mt-2">
-            <p class="text-xs font-semibold">Usuari que Reporta:</p>
-            <p class="text-gray-700 text-sm">{{ report.reporting_user_name }}</p>
-            <p class="text-xs text-gray-500">{{ report.reporting_user_email }}</p>
-          </div>
-          <div class="mt-2">
-            <p class="text-xs font-semibold">Usuari de la Publicació:</p>
-            <p class="text-gray-700 text-sm">{{ report.publication_user_name }}</p>
-            <p class="text-xs text-gray-500">{{ report.publication_user_email }}</p>
-          </div>
-          <div class="mt-2">
-            <p class="text-xs font-semibold">Títol de la Publicació:</p>
-            <p class="text-gray-700 text-sm">{{ report.title }}</p>
-          </div>
-          <div class="mt-2">
-            <p class="text-xs font-semibold">Descripció de la Publicació:</p>
-            <p class="text-gray-700 text-sm">{{ report.description }}</p>
-          </div>
-          <div class="mt-2">
-            <p class="text-xs font-semibold">Informe:</p>
-            <p class="text-gray-700 text-sm">{{ report.report }}</p>
-          </div>
-          <div class="mt-2">
-            <p class="text-xs font-semibold">Imatge Reportada:</p>
-            <select v-model="report.selectedImage" class="appearance-none bg-gray-100 border border-gray-300 text-sm rounded-md focus:outline-none focus:ring-2 focus:ring-blue-400 focus:border-blue-400 p-2 w-full shadow-sm">
-              <option :value="null">Selecciona una imatge</option>
-              <option :value="`${communityUrl}${report.image}`">Veure Imatge</option>
+          <div class="w-1/2 sm:w-1/3">
+            <p class="text-base font-semibold text-gray-900 dark:text-gray-300">Estat:</p>
+            <select v-model="report.status" @change="updateReportStatus(report.id, report.status)" :class="{
+              'bg-yellow-200 dark:bg-yellow-600': report.status === 'pending',
+              'bg-blue-200 dark:bg-blue-600': report.status === 'revising',
+              'bg-green-200 dark:bg-green-600': report.status === 'revised',
+              'appearance-none bg-gray-100 dark:bg-gray-600 dark:text-gray-300 border border-gray-300 dark:border-gray-600 text-sm rounded-md focus:outline-none focus:ring-2 focus:ring-blue-400 focus:border-blue-400 p-2 w-32 shadow-sm': true
+            }">
+              <option value="pending">Pending</option>
+              <option value="revising">Revising</option>
+              <option value="revised">Revised</option>
             </select>
-            <img v-if="report.selectedImage" :src="report.selectedImage" alt="Imatge Reportada" class="w-32 h-auto rounded-md shadow-sm mt-2">
           </div>
+          <div class="w-full sm:w-1/3 mt-4">
+            <p class="text-base font-semibold text-gray-900 dark:text-gray-300">Data:</p>
+            <p class="text-gray-700 dark:text-gray-400 text-sm">{{ report.created_at }}</p>
+          </div>
+        </div>
+        <div class="mb-6">
+          <p class="text-base font-semibold text-gray-900 dark:text-gray-300">Usuari que Reporta:</p>
+          <p class="text-gray-700 dark:text-gray-400 text-sm">{{ report.reporting_user_name }}</p>
+          <p class="text-xs text-gray-500 dark:text-gray-500">{{ report.reporting_user_email }}</p>
+        </div>
+        <div class="mb-6">
+          <p class="text-base font-semibold text-gray-900 dark:text-gray-300">Usuari de la Publicació:</p>
+          <p class="text-gray-700 dark:text-gray-400 text-sm">{{ report.publication_user_name }}</p>
+          <p class="text-xs text-gray-500 dark:text-gray-500">{{ report.publication_user_email }}</p>
+        </div>
+        <div class="mb-6">
+          <p class="text-base font-semibold text-gray-900 dark:text-gray-300">Títol de la Publicació:</p>
+          <p class="text-gray-700 dark:text-gray-400 text-sm">{{ report.title }}</p>
+        </div>
+        <div class="mb-6">
+          <p class="text-base font-semibold text-gray-900 dark:text-gray-300">Descripció de la Publicació:</p>
+          <p class="text-gray-700 dark:text-gray-400 text-sm">{{ report.description }}</p>
+        </div>
+        <div class="mb-6">
+          <p class="text-basee font-semibold text-gray-900 dark:text-gray-300">Informe:</p>
+          <p class="text-gray-700 dark:text-gray-400 text-sm">{{ report.report }}</p>
+        </div>
+        <div class="mb-6">
+          <p class="text-base font-semibold text-gray-900 dark:text-gray-300">Imatge Reportada:</p>
+          <select v-model="report.selectedImage"
+            class="appearance-none bg-gray-100 dark:bg-gray-400 dark:text-gray-100 border border-gray-300 dark:border-gray-600 text-sm rounded-md focus:outline-none focus:ring-2 focus:ring-blue-400 focus:border-blue-400 p-2 w-full shadow-sm">
+            <option :value="null">Selecciona una imatge</option>
+            <option :value="`${communityUrl}${report.image}`">Veure Imatge</option>
+          </select>
+          <img v-if="report.selectedImage" :src="report.selectedImage" alt="Imatge Reportada"
+            class="w-32 h-auto rounded-md shadow-sm mt-2">
         </div>
       </div>
     </div>
-  </template>
-  
-  <script>
-  import { fetchAllReportsPublications, updateReportPublication } from "@/services/communicationManager";
-  
-  export default {
-    data() {
-      return {
-        reports: [],
-        loading: true,
-        communityUrl: import.meta.env.VITE_URL_BACK_COMMUNITY.replace(/\/$/, ''),
-      };
-    },
-    async mounted() {
+  </div>
+</template>
+
+
+<script>
+import { fetchAllReportsPublications, updateReportPublication } from "@/services/communicationManager";
+
+export default {
+  data() {
+    return {
+      reports: [],
+      loading: true,
+      communityUrl: import.meta.env.VITE_URL_BACK_COMMUNITY.replace(/\/$/, ''),
+    };
+  },
+  async mounted() {
+    try {
+      const data = await fetchAllReportsPublications();
+      if (data.error) {
+        console.error(data.error);
+      } else {
+        this.reports = data;
+      }
+    } catch (error) {
+      console.error("Error al obtenir els informes:", error);
+    } finally {
+      this.loading = false;
+    }
+  },
+  methods: {
+    async updateReportStatus(id, status) {
       try {
-        const data = await fetchAllReportsPublications();
-        if (data.error) {
-          console.error(data.error);
+        const response = await updateReportPublication(id, status);
+        if (response.error) {
+          console.error(response.error);
         } else {
-          this.reports = data;
+          console.log("Estat actualitzat correctament");
         }
       } catch (error) {
-        console.error("Error al obtenir els informes:", error);
-      } finally {
-        this.loading = false;
-      }
-    },
-    methods: {
-      async updateReportStatus(id, status) {
-        try {
-          const response = await updateReportPublication(id, status);
-          if (response.error) {
-            console.error(response.error);
-          } else {
-            console.log("Estat actualitzat correctament");
-          }
-        } catch (error) {
-          console.error("Error al actualitzar l'estat:", error);
-        }
+        console.error("Error al actualitzar l'estat:", error);
       }
     }
-  };
-  </script>
+  }
+};
+</script>
--- front/appMentoria/src/components/viewReportUser.vue
+++ front/appMentoria/src/components/viewReportUser.vue
@@ -1,84 +1,93 @@
 <template>
-    <div class="container mx-auto p-3 sm:p-4 bg-white shadow rounded-lg max-w-screen-sm">
-      <h1 class="text-lg sm:text-xl font-bold mb-3">Usuaris Reportats</h1>
-  
-      <div v-for="report in reports" :key="report.id" class="border border-gray-300 rounded-lg p-3 mb-3 shadow-sm">
-        <div class="flex flex-wrap justify-between">
-          <div class="w-1/2 sm:w-1/3">
-            <p class="text-xs font-semibold">Informe Nº:</p>
-            <p class="text-gray-700 text-sm">{{ report.id }}</p>
-          </div>
-          <div class="w-1/2 sm:w-1/3">
-            <p class="text-xs font-semibold">Estat:</p>
-            <select
-              v-model="report.status"
-              @change="updateReportStatus(report.id, report.status)"
-              class="appearance-none bg-gray-100 border border-gray-300 text-sm rounded-md focus:outline-none focus:ring-2 focus:ring-blue-400 focus:border-blue-400 p-2 w-32 shadow-sm"
-            >
-              <option value="pending">Pendent</option>
-              <option value="revising">Revisant</option>
-              <option value="revised">Revisat</option>
-            </select>
-            <p class="text-xs font-semibold mt-2">Data:</p>
-            <p class="text-gray-700 text-sm">{{ report.created_at }}</p>
-          </div>
+  <div class="container mx-auto p-4 bg-gray-100 dark:bg-gray-700 shadow-lg rounded-lg max-w-screen-md">
+    <h1 class="text-2xl font-bold mb-5 text-gray-800 dark:text-gray-100">Usuaris Reportats</h1>
+
+    <div 
+      v-for="report in reports" 
+      :key="report.id" 
+      class="bg-white dark:bg-gray-600 border border-gray-300 dark:border-gray-300 rounded-lg p-5 mb-6 shadow-md"
+    >
+      <div class="flex items-center justify-between mb-4">
+        <div>
+          <p class="text-lg font-semibold text-gray-800 dark:text-gray-300">Informe Nº: {{ report.id }}</p>
         </div>
-        <div class="mt-2">
-          <p class="text-xs font-semibold">Usuari Reportat:</p>
-          <p class="text-gray-700 text-sm">{{ report.reported_user_name }}</p>
-          <p class="text-xs text-gray-500">{{ report.reported_user_email }}</p>
-        </div>
-        <div class="mt-2">
-          <p class="text-xs font-semibold">Usuari que Reporta:</p>
-          <p class="text-gray-700 text-sm">{{ report.reporting_user_name }}</p>
-          <p class="text-xs text-gray-500">{{ report.reporting_user_email }}</p>
-        </div>
-        <div class="mt-2">
-          <p class="text-xs font-semibold">Informe:</p>
-          <p class="text-gray-700 text-sm">{{ report.report }}</p>
+        <div>
+          <p class="text-base font-semibold text-gray-900 dark:text-gray-300">Estat:</p>
+            <select v-model="report.status" @change="updateReportStatus(report.id, report.status)" :class="{
+              'bg-yellow-200 dark:bg-yellow-600': report.status === 'pending',
+              'bg-blue-200 dark:bg-blue-600': report.status === 'revising',
+              'bg-green-200 dark:bg-green-600': report.status === 'revised',
+              'appearance-none bg-gray-100 dark:bg-gray-600 dark:text-gray-300 border border-gray-300 dark:border-gray-600 text-sm rounded-md focus:outline-none focus:ring-2 focus:ring-blue-400 focus:border-blue-400 p-2 w-32 shadow-sm': true
+            }">
+            <option value="pending">Pendent</option>
+            <option value="revising">Revisant</option>
+            <option value="revised">Revisat</option>
+          </select>
         </div>
       </div>
+
+      <div class="mb-5">
+        <p class="text-sm font-semibold text-gray-700 dark:text-gray-100">Data:</p>
+        <p class="text-base text-gray-800 dark:text-gray-200">{{ report.created_at }}</p>
+      </div>
+
+      <div class="mb-5">
+        <p class="text-sm font-semibold text-gray-700 dark:text-gray-100">Usuari Reportat:</p>
+        <p class="text-base text-gray-800 dark:text-gray-200">{{ report.reported_user_name }}</p>
+        <p class="text-sm text-gray-600 dark:text-gray-400">{{ report.reported_user_email }}</p>
+      </div>
+
+      <div class="mb-5">
+        <p class="text-sm font-semibold text-gray-700 dark:text-gray-100">Usuari que Reporta:</p>
+        <p class="text-base text-gray-800 dark:text-gray-200">{{ report.reporting_user_name }}</p>
+        <p class="text-sm text-gray-600 dark:text-gray-400">{{ report.reporting_user_email }}</p>
+      </div>
+
+      <div class="mb-3">
+        <p class="text-sm font-semibold text-gray-700 dark:text-gray-100">Informe:</p>
+        <p class="text-base text-gray-800 dark:text-gray-200">{{ report.report }}</p>
+      </div>
     </div>
-  </template>
-  
-  <script>
-  import { fetchAllUserReports, updateReportUser } from "@/services/communicationManager";
-  
-  export default {
-    data() {
-      return {
-        reports: [],
-        loading: true,
-      };
-    },
-    async mounted() {
+  </div>
+</template>
+
+<script>
+import { fetchAllUserReports, updateReportUser } from "@/services/communicationManager";
+
+export default {
+  data() {
+    return {
+      reports: [],
+      loading: true,
+    };
+  },
+  async mounted() {
+    try {
+      const data = await fetchAllUserReports();
+      if (data.error) {
+        console.error(data.error);
+      } else {
+        this.reports = data;
+      }
+    } catch (error) {
+      console.error("Error al obtenir els informes:", error);
+    } finally {
+      this.loading = false;
+    }
+  },
+  methods: {
+    async updateReportStatus(id, status) {
       try {
-        const data = await fetchAllUserReports();
-        if (data.error) {
-          console.error(data.error);
+        const response = await updateReportUser(id, status);
+        if (response.error) {
+          console.error(response.error);
         } else {
-          this.reports = data;
+          console.log("Estat actualitzat correctament");
         }
       } catch (error) {
-        console.error("Error al obtenir els informes:", error);
-      } finally {
-        this.loading = false;
+        console.error("Error al actualitzar l'estat:", error);
       }
     },
-    methods: {
-      async updateReportStatus(id, status) {
-        try {
-          const response = await updateReportUser(id, status);
-          if (response.error) {
-            console.error(response.error);
-          } else {
-            console.log("Estat actualitzat correctament");
-          }
-        } catch (error) {
-          console.error("Error al actualitzar l'estat:", error);
-        }
-      },
-    },
-  };
-  </script>
-  
+  },
+};
+</script>
